ad82e123ff0d87d03c2d7908479b508f5946bfa7
actionxbar-14 <ankitpaths144@gmail.com>
2026-03-20 00:35:56 +0530

    P2_Square_root added!!

---
+++ Basic_Programs/P2_Square_root.py
@@ -0,0 +1,34 @@
+
+# Program : Python Program to find the Square Root. 
+
+
+
+
+
+
+# :: Solution 1 : using exponentiation method. 
+
+# num = int(input("Enter the num:"))
+
+# Square_root = num ** (1/2)
+
+# print(f"The Square root of the {num} is :{Square_root}")
+
+
+
+
+
+
+
+
+
+#:: Solution 2 : using math module
+
+
+# import math
+
+# num = int(input("Enter the num : "))
+
+# square_num =  math.sqrt(num)
+
+# print(f" The square root of the num : {num} is {square_num}")
