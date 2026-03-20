524d049391007b5097edaa1e73291fc3746a7068
actionxbar-14 <ankitpaths144@gmail.com>
2026-03-21 01:39:42 +0530

    P4 extended!!

--- Basic_Programs/P4_Swapping.py
+++ Basic_Programs/P4_Swapping.py
@@ -28,3 +28,17 @@
 # num1 = num2 - num1
 # print(f"After Swapping : num1 = {num1} and num2 = {num2}")
     
+
+
+
+
+num1  = int(input("Enter num1 : "))
+num2  = int(input("Enter num2 :"))
+
+print(f"Before Swapping : num1 = {num1} and num2 = {num2}")
+
+num1 , num2 = num2 , num1
+
+print(f"After Swapping : num1 = {num1} and num2 = {num2}")
+
+
