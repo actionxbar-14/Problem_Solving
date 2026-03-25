b1fce9099dc53d0e8eb73ff9b82a131a4a001f84
actionxbar-14 <ankitpaths144@gmail.com>
2026-03-26 01:43:18 +0530

    practice....

---
+++ Basic_Programs/Practice.py
@@ -0,0 +1,41 @@
+
+# num = int(input("Enter the num :"))
+
+# if num == 1:
+#     print(f"{num} is not a prime number")
+    
+# if num > 1:
+#     for i in range(2,num):
+#         if num%i == 0:
+#             print(f"{num} is not a prime number")
+#             break
+            
+#     else:
+#         print(f"{num} is a prime number")        
+            
+
+
+
+# __________________________________________________________________________________________________________________
+
+
+
+
+
+
+# num = int(input("Enter the num :"))
+
+# if num == 1:
+#     print(f"{num} is not a prime number")
+    
+
+# if num > 1:
+#     for i in range(2,num):
+#         if num%i == 0:
+#             print(f"{num} is not a prime number")
+#             break 
+        
+#     else:
+#         print(f"{num} is a prime number")
+        
+        
