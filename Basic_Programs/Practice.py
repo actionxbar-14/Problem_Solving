04062e77174688a8979747ffc5df1280658090cf
actionxbar-14 <ankitpaths144@gmail.com>
2026-03-27 01:48:17 +0530

    practice....

--- Basic_Programs/Practice.py
+++ Basic_Programs/Practice.py
@@ -43,3 +43,27 @@
 
             
         
+        
+        
+        
+        
+lower_num = int(input("Enter the lower_num :"))
+upper_num = int(input("Enter the upper_num :"))
+
+for num in range(lower_num , upper_num+1):
+    if num == 1:
+        print(f"{num} is not a prime number")
+        
+    if num > 1:
+        for i in range(2, num):
+            
+            if num % i == 0:
+                print(f"{num} is not a prime number")
+                break 
+        
+        else:
+            print(f"{num} is a prime number")
+                
+            
+                
+                
