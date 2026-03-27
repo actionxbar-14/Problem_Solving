0cf269fa5e0d8327a065a6bb69a1148eb8a7e15c
actionxbar-14 <ankitpaths144@gmail.com>
2026-03-28 02:27:03 +0530

    P13 done!!

---
+++ Basic_Programs/P13_Celsius_to_Fahrenheit.py
@@ -0,0 +1,11 @@
+
+
+# :: Program : Program to Convert Celsius to Fahrenheit. 
+
+
+
+user_celsius = float(input("Enter temperature (in celsuis) :"))
+
+temp_fahrenheit = (user_celsius * (9/5)) + 32
+
+print(f"the {user_celsius} degree celsius is = {temp_fahrenheit} fahrenheit")
--- Basic_Programs/Practice.py
+++ Basic_Programs/Practice.py
@@ -47,23 +47,76 @@
         
         
         
-lower_num = int(input("Enter the lower_num :"))
-upper_num = int(input("Enter the upper_num :"))
+# lower_num = int(input("Enter the lower_num :"))
+# upper_num = int(input("Enter the upper_num :"))
 
-for num in range(lower_num , upper_num+1):
-    if num == 1:
-        print(f"{num} is not a prime number")
+# for num in range(lower_num , upper_num+1):
+#     if num == 1:
+#         print(f"{num} is not a prime number")
         
-    if num > 1:
-        for i in range(2, num):
+#     if num > 1:
+#         for i in range(2, num):
             
-            if num % i == 0:
-                print(f"{num} is not a prime number")
-                break 
+#             if num % i == 0:
+#                 print(f"{num} is not a prime number")
+#                 break 
         
-        else:
-            print(f"{num} is a prime number")
+#         else:
+#             print(f"{num} is a prime number")
                 
             
+             
+                
+      
+    
+# num = int(input("Enter the num :"))
+
+# if num == 1:
+#     print(f"{num} is not a prime number")
+    
+# if num > 1:
+#     for i in range(2,num):
+#         if num % i == 0:
+#             print(f"{num} is not a prime number")
+#             break 
+        
+#     else:
+#         print(f"{num} is a prime number")   
+
+
+
+
+
+
+
+
+
+
+
+# print("_________Prime number in an interval_________")
+
+# starting_num = int(input("Enter the starting num :"))
+# ending_num = int(input("Enter the ending num :"))
+
+
+# for num in range(starting_num , ending_num):
+#     if num == 1:
+#         print(f"{num} is not a prime number")
+        
+#     if num > 1:
+#         for i in range(2,num):
+#             if num % i == 0:
+#                 print(f"{num} is not a prime number")
+#                 break 
+            
+#         else:
+#             print(f"{num} is a prime number")
+            
+            
+            
+
                 
                 
+        
+            
+    
